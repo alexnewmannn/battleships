c6bea692370e77ab19c34b7f1bb0fa04b9ae263a
Alex Newman <Alex.newman@bedegaming.com>
2015-02-25 00:10:28 +0000

    alpha

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var game = new Phaser.Game(500, 500, Phaser.AUTO, 'battleships', { preload: prel
 
 var map;
 var layer1;
-
+var test;
 
 function preload() {
 	game.load.image('test', 'assets/tilemap.png');
@@ -18,18 +18,24 @@ function create() {
 	layer1 = map.create('tiles', 10, 10, 50, 50);
 	layer1.resizeWorld();
 
+	layer2 = map.createBlankLayer('layer2', 10, 10, 50, 50);
+
 
 	for (var y = 0; y < 50; y++) {
 		for (var i = 0; i < 50; i++) {
-			console.log(i, y)
 			map.putTile(0, i, y, layer1)
 		}
 	}
+
+	map.putTile(1, 2, 3, layer2)
+	map.putTile(1, 3, 3, layer2)
+	map.putTile(1, 4, 3, layer2)
+
+	layer2.alpha = 0.3
 }
 
 function update() {
 	if (game.input.mousePointer.isDown) {
-		// console.log(layer1.getTileX(game.input.activePointer.worldX * 50))
 	}
 }
 
