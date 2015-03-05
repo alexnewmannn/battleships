33246f910675f8ff55a15f22caf88aa41e661222
Alex Newman <Alex.newman@bedegaming.com>
2015-03-05 01:43:07 +0000

    remove unecessary layers

--- js/main.js
+++ js/main.js
@@ -41,17 +41,16 @@ function create() {
 	layer1.resizeWorld();
 	layer1.inputEnabled = true;
 
-	layer2 = map.createBlankLayer('layer2', 10, 10, 50, 50);
 
 	for (var y = 0; y < 50; y++) {
 		for (var i = 0; i < 50; i++) {
-			map.putTile(0, i, y, layer2)
+			map.putTile(0, i, y);
 		}
 	}
 
-	renderShips();
+	plotShips();
 
-	// layer2.alpha = 0.9;
+	layer1.alpha = 0.9;
 }
 
 var shipBoundaries;
@@ -61,41 +60,31 @@ var yAxisPoints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var coords = [];
 var jointcoords = [];
 
-function renderShips() {
+function plotShips() {
 	for (var i = 0; i < ships.length; i++) { // this is for each ship
-		shipBoundaries = Math.floor(10 - ships[i][0])
+		shipBoundaries = Math.floor(10 - ships[i][0]);
 		xAxis = Math.floor(Math.random() * shipBoundaries);
 		yAxis = yAxisPoints[Math.floor(Math.random() * yAxisPoints.length)];
 		grid[yAxis].splice(xAxis, ships[i][0]); //delete the length of the ship starting from the start position
 		yAxisPoints.splice(yAxis, 1);
 
-		for (var y = 1; y < ships[i][0] + 1; y++) { // this is the ship size
-			// randomNum = Math.round(Math.random();
-			// console.log(randomNum)
-			map.putTile(Math.round(Math.random()) + 1, xAxis, yAxis, layer1);
-			
+		for (var y = 1; y < ships[i][0] + 1; y++) {
 			jointcoords = { 
 				'name': [xAxis, yAxis], 
 				Y: yAxis, 
 				X: xAxis 
 			};
+
 			coords.push(jointcoords);
 			xAxis += 1;
 		}
 	}
-
-
-	// each random value should be removed from an array 
-	// to avoid it being used again later (will later sort exceptions out, i.e they 
-	// can be on same row if the distance between is greater than 2)
-	// each x and y coord should be pushed to an array within an array
-	// and be checked via - array[i][0] && array[i][0] were clicked 
 }
 
 var clicked = [];
 
 
-function test() {
+function renderTile() {
 	var mouseX = game.input.activePointer.worldX;
 	var mouseY = game.input.activePointer.worldY;
 	var tileX = layer1.getTileX(mouseX);
@@ -108,17 +97,20 @@ function test() {
 	}
 
 	for (var i = 0; i < coords.length; i++) {
-		// console.log(clicked)
 		if (tileX === coords[i].X && tileY === coords[i].Y) {
-			map.removeTile(layer1.getTileX(mouseX), layer1.getTileY(mouseY), layer2)
+			map.putTile(1, tileX, tileY);
+
+			fx.stop('boom');
 			fx.play('boom');
+
 			break;
 		} else {
-			map.putTile(3, layer1.getTileX(mouseX), layer1.getTileY(mouseY), layer2)
+			map.putTile(3, layer1.getTileX(mouseX), layer1.getTileY(mouseY), layer1);
 		}
 
 		// If the for loop ends and the last loop isnt a hit, play the sound to avoid looping
 		if (i + 1 === coords.length && tileX !== coords[i].X) {
+			fx.stop('noo');
 			fx.play('noo');
 		}
 
@@ -127,7 +119,7 @@ function test() {
 }
 
 function update() {
-	layer1.events.onInputDown.add(test, this)
+	layer1.events.onInputDown.add(renderTile, this);
 }
 
 function render() {
